70faedda5eeb8b9796206ef20a12afb4925ea468
Jacqueline Omollo <jacquelineOmolloo@gmail.com>
2019-08-04 21:32:47 -0400

    Data Pulled In

--- starwars/src/components/DataCard.js
+++ starwars/src/components/DataCard.js
@@ -1,7 +1,30 @@
 import React from "react";
 
 const DataCard = props => {
-  return <div />;
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <p>
+        <strong>"Birthday Year:</strong> {props.birthday}
+      </p>
+      <p>
+        <strong>"Height:</strong> {props.height}
+      </p>
+      <p>
+        <strong>"Mass:</strong> {props.mass}
+      </p>
+      <p>
+        <strong>"Eye Color:</strong> {props.eyeColor}
+      </p>
+
+      <p>
+        <strong>"Hair Color:</strong> {props.hairColor}
+      </p>
+      <p>
+        <strong>"Gender:</strong> {props.gender}
+      </p>
+    </div>
+  );
 };
 
 export default DataCard;
