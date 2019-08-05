98f1d41743738d5c465b691a6dd1dcd05c1f0f64
Jacqueline Omollo <jacquelineOmolloo@gmail.com>
2019-08-05 00:00:46 -0400

    more changes to design

--- starwars/src/components/DataCard.js
+++ starwars/src/components/DataCard.js
@@ -1,35 +1,42 @@
 import React from "react";
-import { Card } from "semantic-ui-react";
+import { Card, Grid } from "semantic-ui-react";
 const DataCard = props => {
   return (
-    <Card>
-      <Card.Content>
-        <Card.Header>
-          <h1>{props.name}</h1>
-        </Card.Header>
-        <Card.Description>
-          <p>
-            <strong>"Birthday Year:</strong> {props.birthday}
-          </p>
-          <p>
-            <strong>"Height:</strong> {props.height}
-          </p>
-          <p>
-            <strong>"Mass:</strong> {props.mass}
-          </p>
-          <p>
-            <strong>"Eye Color:</strong> {props.eyeColor}
-          </p>
+    <Grid columns={3} divided>
+      <Grid.Row>
+        <Grid.Column>
+          <Card>
+            <Card.Content>
+              <Card.Header>
+                <h1>{props.name}</h1>
+              </Card.Header>
 
-          <p>
-            <strong>"Hair Color:</strong> {props.hairColor}
-          </p>
-          <p>
-            <strong>"Gender:</strong> {props.gender}
-          </p>
-        </Card.Description>
-      </Card.Content>
-    </Card>
+              <Card.Description>
+                <p>
+                  <strong>"Birthday Year:</strong> {props.birthday}
+                </p>
+                <p>
+                  <strong>"Height:</strong> {props.height}
+                </p>
+                <p>
+                  <strong>"Mass:</strong> {props.mass}
+                </p>
+                <p>
+                  <strong>"Eye Color:</strong> {props.eyeColor}
+                </p>
+
+                <p>
+                  <strong>"Hair Color:</strong> {props.hairColor}
+                </p>
+                <p>
+                  <strong>"Gender:</strong> {props.gender}
+                </p>
+              </Card.Description>
+            </Card.Content>
+          </Card>
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
   );
 };
 
