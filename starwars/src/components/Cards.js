0c87324f17aa694b7f071381a6a54a3eb15a8419
Jacqueline Omollo <jacquelineOmolloo@gmail.com>
2019-08-04 21:24:55 -0400

    first  setup commit

---
+++ starwars/src/components/Cards.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+import DataCard from "../components/DataCard";
+import axios from "axios";
+
+const Cards = () => {
+  const [peopleData, setData] = useState([]);
+
+
+
+
+export default Cards;
---
+++ starwars/src/components/DataCard.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DataCard = props => {
+  return <div />;
+};
+
+export default DataCard;
