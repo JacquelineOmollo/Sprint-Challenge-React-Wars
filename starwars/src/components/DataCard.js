b43f38264251e7c32009c225e76d534bde00e251
Jacqueline Omollo <jacquelineOmolloo@gmail.com>
2019-08-04 23:33:22 -0400

    had promlems with semantic intall but managed to a little styling

--- starwars/src/components/DataCard.js
+++ starwars/src/components/DataCard.js
@@ -1,29 +1,35 @@
 import React from "react";
-
+import { Card } from "semantic-ui-react";
 const DataCard = props => {
   return (
-    <div>
-      <h1>{props.name}</h1>
-      <p>
-        <strong>"Birthday Year:</strong> {props.birthday}
-      </p>
-      <p>
-        <strong>"Height:</strong> {props.height}
-      </p>
-      <p>
-        <strong>"Mass:</strong> {props.mass}
-      </p>
-      <p>
-        <strong>"Eye Color:</strong> {props.eyeColor}
-      </p>
+    <Card>
+      <Card.Content>
+        <Card.Header>
+          <h1>{props.name}</h1>
+        </Card.Header>
+        <Card.Description>
+          <p>
+            <strong>"Birthday Year:</strong> {props.birthday}
+          </p>
+          <p>
+            <strong>"Height:</strong> {props.height}
+          </p>
+          <p>
+            <strong>"Mass:</strong> {props.mass}
+          </p>
+          <p>
+            <strong>"Eye Color:</strong> {props.eyeColor}
+          </p>
 
-      <p>
-        <strong>"Hair Color:</strong> {props.hairColor}
-      </p>
-      <p>
-        <strong>"Gender:</strong> {props.gender}
-      </p>
-    </div>
+          <p>
+            <strong>"Hair Color:</strong> {props.hairColor}
+          </p>
+          <p>
+            <strong>"Gender:</strong> {props.gender}
+          </p>
+        </Card.Description>
+      </Card.Content>
+    </Card>
   );
 };
 
